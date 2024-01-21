acafba0830fa6409a187a498634bbbaaf3443ba9
Andrii Oriekhov <andriyorehov@gmail.com>
2024-01-21 20:15:09 +0200

    add support of helper

--- test/test-project-expected/with-helper/index.ts
+++ test/test-project-expected/with-helper/index.ts
@@ -1,10 +1,10 @@
 import { ObjectValues } from "./types/object-values";
 
 export const Direction = {
-  Up: "UP",
-  Down: "DOWN",
-  Left: "LEFT",
-  Right: "RIGHT",
+    Up: "UP",
+    Down: "DOWN",
+    Left: "LEFT",
+    Right: "RIGHT",
 } as const;
 
 export type Direction = ObjectValues<typeof Direction>;
